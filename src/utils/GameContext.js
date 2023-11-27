2c3896df3ffab60bd139b3b3c81ee5d1c8edc1ed
vandana104 <vandanaoel@gmail.com>
2023-11-27 20:09:54 +0530

    Updated

--- src/utils/GameContext.js
+++ src/utils/GameContext.js
@@ -1,5 +1,3 @@
-// gamecontext.js
-
 import React, { createContext, useContext, useReducer } from "react";
 
 const initialGameState = {
@@ -21,41 +19,36 @@ const checkWin = (state) => {
   return null;
 };
 
-// ... (previous code)
-
 const gameReducer = (state, action) => {
-    switch (action.type) {
-      case "GAME_WON":
-        return {
-          ...state,
-          gamesPlayed: state.gamesPlayed + 1,
-          gamesWon: state.gamesWon + 1,
-        };
-      case "GAME_LOST":
-        return {
-          ...state,
-          gamesPlayed: state.gamesPlayed + 1,
-          gamesLost: state.gamesLost + 1,
-        };
-      case "INCREMENT_SCORE":
-        const newScore = state.score + action.points;
-        const movesRemaining = state.movesRemaining - 1;
-        const winStatus =
-          checkWin({ ...state, score: newScore, movesRemaining }) ||
-          (movesRemaining === 0 && newScore >= 1125 ? "won" : null);
-        return {
-          ...state,
-          score: newScore,
-          movesRemaining,
-          winStatus,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  // ... (remaining code)
-  
+  switch (action.type) {
+    case "GAME_WON":
+      return {
+        ...state,
+        gamesPlayed: state.gamesPlayed + 1,
+        gamesWon: state.gamesWon + 1,
+      };
+    case "GAME_LOST":
+      return {
+        ...state,
+        gamesPlayed: state.gamesPlayed + 1,
+        gamesLost: state.gamesLost + 1,
+      };
+    case "INCREMENT_SCORE":
+      const newScore = state.score + action.points;
+      const movesRemaining = state.movesRemaining - 1;
+      const winStatus =
+        checkWin({ ...state, score: newScore, movesRemaining }) ||
+        (movesRemaining === 0 && newScore >= 1125 ? "won" : null);
+      return {
+        ...state,
+        score: newScore,
+        movesRemaining,
+        winStatus,
+      };
+    default:
+      return state;
+  }
+};
 
 const GameProvider = ({ children }) => {
   const [state, dispatch] = useReducer(gameReducer, initialGameState);
